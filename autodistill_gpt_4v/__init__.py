c929ed049db4330560bfbb9bd922b3b2fc31bac5
James Gallagher <jamesg@jamesg.blog>
2023-11-23 14:27:48 +0000

    add 0.1.5 code

--- autodistill_gpt_4v/__init__.py
+++ autodistill_gpt_4v/__init__.py
@@ -1,3 +1,3 @@
 from autodistill_gpt_4v.gpt4v_model import GPT4V
 
-__version__ = "0.1.1"
+__version__ = "0.1.5"
--- autodistill_gpt_4v/gpt4v_model.py
+++ autodistill_gpt_4v/gpt4v_model.py
@@ -9,52 +9,124 @@ from openai import OpenAI
 
 HOME = os.path.expanduser("~")
 
-# def processed_predictions(response, ontology, height, width):
-#     predictions = response.choices[0].message.content.split("\n")
-
-#     if "None" in predictions:
-#         return sv.Detections.empty()
-
-#     xyxys = predictions[0].split(", ")
-#     class_ids = [0]
-
-#     xyxys = [float(x) for x in xyxys]
-
-#     # normalize from 0-1 to image coordinates
-#     # coordinates are x_min, y_min, x_max, y_max
-#     xyxys[0] = xyxys[0] * height
-#     xyxys[1] = xyxys[1] * width
-#     xyxys[2] = xyxys[2] * height
-#     xyxys[3] = xyxys[3] * width
-
-#     xyxys = [int(xyxy) for xyxy in xyxys]
-
-#     # xyxy_coordinates = [obj[:-1] for obj in xyxys]
-
-#     return sv.Detections(
-#         xyxy=np.array([xyxys]),
-#         confidence=np.array([1]),
-#         class_id=np.array(class_ids),
-#     )
-
-
 @dataclass
 class GPT4V(DetectionBaseModel):
     ontology: CaptionOntology
 
-    def __init__(self, ontology: CaptionOntology):
-        self.client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    def __init__(self, ontology: CaptionOntology, api_key, prompt: str = None):
+        self.client = OpenAI(api_key=api_key)
         self.ontology = ontology
-        pass
+        self.prompt = prompt
 
-    def predict(self, input, classes) -> sv.Classifications:
+    def set_of_marks(self, input, masked_input, classes, masks) -> sv.Detections:
+        if classes is None:
+            classes = {k:k for k in self.ontology.prompts()}
+
         payload = [
             {
                 "role": "user",
                 "content": [
                     {
                         "type": "text",
-                        "text": f"What is in the image? Return the class of the object in the image. Here are the classes: {', '.join(classes)}. You can only return one class from that list.",
+                        "text": f"Attached is an image and a set of selections for this image. Please return the number of any selections for a {', '.join(classes)}. Return each class on a new line like Banana: 1, 2, 3 [new line] Apple: 5, 7, 8."
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,"
+                            + base64.b64encode(open(input, "rb").read()).decode(
+                                "utf-8"
+                            ),
+                        }
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,"
+                            + base64.b64encode(open(masked_input, "rb").read()).decode(
+                                "utf-8"
+                            ),
+                        }
+                    },
+                ],
+            }
+        ]
+
+        print(payload[0]["content"][0])
+
+        response = self.client.chat.completions.create(
+            model="gpt-4-vision-preview",
+            messages=payload,
+            max_tokens=300,
+        )
+
+        print(response)
+
+        if "none" not in self.ontology.prompts():
+            self.ontology.prompts().append("none")
+
+        class_map = {}
+        
+        for line in response.choices[0].message.content.split("\n"):
+            if ":" in line:
+                class_name = line.split(":")[0].strip().lower()
+
+                if class_name not in self.ontology.prompts():
+                    continue
+                
+                if class_name not in class_map:
+                    class_map[class_name] = []
+                
+                try:
+                    int(line.split(":")[1].strip().lower())
+                except:
+                    continue
+
+                class_map[class_name].append(int(line.split(":")[1].strip().lower()))
+
+        # get ids from class_map
+        all_ids = [item for sublist in class_map.values() for item in sublist]
+
+        print(all_ids)
+
+        # change class id for each mask
+        for idx, _ in enumerate(masks.xyxy):
+            for class_name, mask_ids in class_map.items():
+                for mask_id in mask_ids:
+                    if mask_id == idx:
+                        masks.class_id[idx] = self.ontology.prompts().index(class_name)
+                        break
+
+        new_masks = []
+        new_class_id = []
+        new_xyxy = []
+
+        for idx, item in enumerate(masks.class_id):
+            if item in all_ids:
+                new_masks.append(masks.mask[idx])
+                new_class_id.append(masks.class_id[idx])
+                new_xyxy.append(masks.xyxy[idx])
+
+        masks.confidence = np.array([1] * len(masks.class_id))
+
+        return sv.Detections(
+            xyxy=np.array(new_xyxy),
+            mask=np.array(new_masks),
+            class_id=np.array(new_class_id),
+            confidence=np.array([1] * len(new_masks))
+        )
+
+    def predict(self, input, classes: list = None) -> sv.Classifications:
+        if classes is None:
+            classes = {k:k for k in self.ontology.prompts()}
+
+        payload = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": f"What is in the image? Return the class of the object in the image. Here are the classes: {', '.join(classes)}. You can only return one class from that list." if self.prompt is None else self.prompt
                     },
                     {
                         "type": "image_url",
@@ -75,49 +147,34 @@ class GPT4V(DetectionBaseModel):
             max_tokens=300,
         )
 
-        class_ids = self.ontology.prompts().index(response.choices[0].message.content)
+        if "none" not in self.ontology.prompts():
+            self.ontology.prompts().append("none")
 
+        if self.prompt:
+            class_ids = []
+            
+            for line in response.choices[0].message.content.split("\n"):
+                if ":" in line:
+                    class_name = line.split(":")[1].strip().lower()
+                    print(class_name)
+                    if class_name not in self.ontology.prompts():
+                        class_ids.append(self.ontology.prompts().index("none"))
+                    else:
+                        class_ids.append(self.ontology.prompts().index(class_name))
+                    
+                    print(line)
+
+            confidence = [1] * len(class_ids)
+        else:
+            result = response.choices[0].message.content.lower()
+            if result not in self.ontology.prompts():
+                class_ids.append(self.ontology.prompts().index("none"))
+                confidence = [0]
+            else:
+                class_ids = [self.ontology.prompts().index(result)]
+                confidence = [1]
+        
         return sv.Classifications(
-            class_id=np.array([class_ids]),
-            confidence=np.array([1]),
+            class_id=np.array(class_ids),
+            confidence=np.array(confidence),
         )
-
-    # def predict(self, input: str) -> sv.Detections:
-        # payload = [
-        #     {
-        #         "role": "user",
-        #         "content": [
-        #             {
-        #                 "type": "text",
-        #                 "text": f"Can you tell me the location of the dog on the image. Share the x_min, y_min, x_max and y_max in 0-1 normalized space. Only return the numbers, nothing else."
-        #             },
-        #             {
-        #                 "type": "image_url",
-        #                 "image_url": {
-        #                     "url": f"data:image/jpeg;base64,"
-        #                     + base64.b64encode(open(input, "rb").read()).decode(
-        #                         "utf-8"
-        #                     ),
-        #                 },
-        #             },
-        #         ],
-        #     }
-        # ]
-
-        # print(payload[0]["content"][0])
-
-        # from PIL import Image
-
-        # height, width = Image.open(input).size
-
-        # # exit()
-
-        # response = self.client.chat.completions.create(
-        #     model="gpt-4-vision-preview",
-        #     messages=payload,
-        #     max_tokens=300,
-        # )
-
-        # print(response)
-
-        # return processed_predictions(response, self.ontology, height, width)
